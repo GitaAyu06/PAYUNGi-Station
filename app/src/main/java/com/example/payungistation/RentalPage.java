ccefb78964b5a11f68676b12dc9fbd3dccd012cf
GitaAyu06 <gita.ayu73@ui.ac.id>
2020-05-07 11:25:41 +0800

    Scanner Working

--- app/src/main/java/com/example/payungistation/RentalPage.java
+++ app/src/main/java/com/example/payungistation/RentalPage.java
@@ -1,24 +1,98 @@
 package com.example.payungistation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class RentalPage extends AppCompatActivity  {
     private Button btnHome;
+    private ImageView imgScan;
+    private TextView textView;
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
+    private BarcodeDetector barcodeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rental_page);
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         btnHome = findViewById(R.id.btnHome);
+        surfaceView = findViewById(R.id.cameraPreview);
+        textView = findViewById(R.id.textView);
 
+        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
+        cameraSource = new CameraSource.Builder(this,barcodeDetector).setRequestedPreviewSize(640,480).
+                setFacing(1).setAutoFocusEnabled(true).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                try{
+                    cameraSource.start(holder);
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCode = detections.getDetectedItems();
+
+                if(qrCode.size() != 0){
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(qrCode.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
